6671f6af2457e8b551c8bcbadbc5b1cca37841c8
Faye Hayes <fhayes30@gmail.com>
2018-01-07 17:17:14 -0800

    update prod - debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -107,6 +107,16 @@ Rails.application.configure do
   #       :openssl_verify_mode  => 'none'
   #     }
 
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'yourdomain.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
