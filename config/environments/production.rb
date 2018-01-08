1a8bc10431e20829efb3cc0065e70c31d6674b6e
Faye Hayes <fhayes30@gmail.com>
2018-01-07 17:12:02 -0800

    update production to take out sendgrid debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,25 +87,25 @@ Rails.application.configure do
   end
 
   # email
-
-  config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.perform_caching = true
-    config.action_mailer.default_url_options = { :host => 'www.fayemyrettehayes.com' }
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.smtp_settings = {
-        address:              'smtp.sendgrid.net',
-        port:                 587,
-        domain:               'gmail.com',
-        authentication:       :plain,
-        user_name:            ENV['SENDGRID_USERNAME'],
-        password:             ENV['SENDGRID_PASSWORD'],
-        domain:               'heroku.com'
-        enable_starttls_auto: true,
-        :openssl_verify_mode  => 'none'
-      }
+  #
+  # config.action_mailer.raise_delivery_errors = true
+  #
+  # config.action_mailer.perform_caching = true
+  #   config.action_mailer.default_url_options = { :host => 'www.fayemyrettehayes.com' }
+  #   config.action_mailer.delivery_method = :smtp
+  #   config.action_mailer.perform_deliveries = true
+  #   config.action_mailer.raise_delivery_errors = true
+  #   config.action_mailer.smtp_settings = {
+  #       address:              'smtp.sendgrid.net',
+  #       port:                 587,
+  #       domain:               'gmail.com',
+  #       authentication:       :plain,
+  #       user_name:            ENV['SENDGRID_USERNAME'],
+  #       password:             ENV['SENDGRID_PASSWORD'],
+  #       domain:               'heroku.com'
+  #       enable_starttls_auto: true,
+  #       :openssl_verify_mode  => 'none'
+  #     }
 
 
   # Do not dump schema after migrations.
